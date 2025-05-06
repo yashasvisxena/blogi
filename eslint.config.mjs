72a2106f293435ee12476ec02fc8a52873276720
yashasvisxena <yashasvi1802@gmail.com>
2025-05-06 23:31:00 +0530

    refactor: consolidate ESLint configuration by removing .eslintrc.json
    - Deleted the .eslintrc.json file and integrated its rules into eslint.config.mjs for better organization.
    - Updated ESLint configuration to include specific TypeScript rules directly within the extends array.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+    extends: ["next"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
